185d0f5fafa263c1a5c2549459d75d1da245de1c
bayazidsustami <bayazidsustamy@gmail.com>
2024-03-18 23:08:17 +0700

    feat : crate jwt handler

---
+++ security/jwt_handler.go
@@ -0,0 +1,26 @@
+package security
+
+import (
+	jwtware "github.com/gofiber/contrib/jwt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/spf13/viper"
+)
+
+func GetJwtTokenHandler() fiber.Handler {
+	return jwtware.New(jwtware.Config{
+		SigningKey: jwtware.SigningKey{Key: []byte(viper.GetString("JWT_SECRET"))},
+		ContextKey: "userInfo",
+		ErrorHandler: func(c *fiber.Ctx, err error) error {
+			return fiber.NewError(fiber.StatusForbidden, err.Error())
+		},
+	})
+}
+
+func CheckTokenHeaderExist(ctx *fiber.Ctx) error {
+	authHeader := ctx.Get("Authorization")
+	if authHeader == "" {
+		return fiber.NewError(fiber.StatusUnauthorized, "Unauthorized")
+	} else {
+		return ctx.Next()
+	}
+}
