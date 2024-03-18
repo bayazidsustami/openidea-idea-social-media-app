bb4edc77eba311fa23a11b8cf27b385c886a55f5
Bayazid Sustami <43400782+bayazidsustami@users.noreply.github.com>
2024-03-18 23:32:03 +0700

    Feature/yazid/create boilerplate code (#1)
    * feat : add gofiber
    
    * feat : setup gofiber
    
    * feat : create binding env
    
    * feat : create register router
    
    * feat : add goccy json encoder
    
    * feat : crate jwt handler
    
    * feat : create db connection pool
    
    * feat : add aws config s3 session
    
    * feat : add bind s3 region

---
+++ app/fiber_app.go
@@ -0,0 +1,28 @@
+package app
+
+import (
+	"log"
+	"openidea-idea-social-media-app/config"
+
+	"github.com/goccy/go-json"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func StartFiberApp() {
+	app := fiber.New(fiber.Config{
+		JSONEncoder:  json.Marshal,
+		JSONDecoder:  json.Unmarshal,
+		IdleTimeout:  config.IdleTimeout,
+		WriteTimeout: config.WriteTimeout,
+		ReadTimeout:  config.ReadTimeout,
+		Prefork:      true,
+	})
+
+	app.Use(logger.New())
+
+	RegisterRoute(app)
+
+	err := app.Listen("localhost:8000")
+	log.Fatal(err)
+}
---
+++ config/env_binder.go
@@ -0,0 +1,33 @@
+package config
+
+import "github.com/spf13/viper"
+
+func init() {
+	envBinder()
+}
+
+func envBinder() {
+	/**
+	 * Env variablees related to database
+	 */
+	viper.BindEnv("DB_NAME")
+	viper.BindEnv("DB_USERNAME")
+	viper.BindEnv("DB_PASSWORD")
+	viper.BindEnv("DB_HOST")
+	viper.BindEnv("DB_PORT")
+
+	/**
+	 * Env variables related to AWS S3
+	 */
+	viper.BindEnv("S3_ID")
+	viper.BindEnv("S3_SECRET_KEY")
+	viper.BindEnv("S3_BUCKET_NAME")
+	viper.BindEnv("S3_REGION")
+
+	/**
+	 * Env variable related to encrypt and hash
+	 */
+	viper.BindEnv("BCRYPT_SALT")
+	viper.BindEnv("JWT_SECRET")
+
+}
