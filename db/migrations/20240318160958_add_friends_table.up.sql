ca20508fce985da002caa5564639ebd831a0f050
Fiqar Aprialim <fiqarapr@gmail.com>
2024-03-18 23:22:46 +0700

    feat: migrations for setup tables

---
+++ db/migrations/20240318160958_add_friends_table.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS friends (
+  friend_id SERIAL PRIMARY KEY NOT NULL,
+	user_id_requester INTEGER NOT NULL,
+	user_id_accepter INTEGER NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
