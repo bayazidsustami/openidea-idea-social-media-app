8eb9df6e5163a76cd00db35345c07fef99fa0de2
bayazidsustami <bayazidsustamy@gmail.com>
2024-03-19 22:56:01 +0700

    chore : remove child on deleted

--- db/migrations/20240318160958_add_friends_table.up.sql
+++ db/migrations/20240318160958_add_friends_table.up.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS friends (
 	user_id_requester INTEGER NOT NULL,
 	user_id_accepter INTEGER NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (user_id_requester) REFERENCES users (user_id),
-  FOREIGN KEY (user_id_accepter) REFERENCES users (user_id)
+  FOREIGN KEY (user_id_requester) REFERENCES users (user_id) ON DELETE CASCADE,
+  FOREIGN KEY (user_id_accepter) REFERENCES users (user_id) ON DELETE CASCADE
 );
