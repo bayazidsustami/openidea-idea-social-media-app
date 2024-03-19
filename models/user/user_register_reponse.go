23be1ecef95ad270e17c80e0d17eadab4fe111c8
bayazidsustami <bayazidsustamy@gmail.com>
2024-03-19 09:57:29 +0700

    feat : crete register req and res

---
+++ models/user/user_register_reponse.go
@@ -0,0 +1,30 @@
+package user_model
+
+type UserRegisterResponse[T UserData] struct {
+	Message string `json:"message"`
+	Data    T      `json:"data"`
+}
+
+type UserData interface {
+	GetName() string
+}
+
+type UserEmailDataResponse struct {
+	Email       string `json:"email"`
+	Name        string `json:"name"`
+	AccessToken string `json:"accessToken"`
+}
+
+func (ue *UserEmailDataResponse) GetName() string {
+	return ue.Name
+}
+
+type UserPhoneDataResponse struct {
+	Phone       string `json:"phone"`
+	Name        string `json:"name"`
+	AccessToken string `json:"accessToken"`
+}
+
+func (up *UserPhoneDataResponse) GetName() string {
+	return up.Name
+}
---
+++ models/user/user_register_request.go
@@ -0,0 +1,8 @@
+package user_model
+
+type UserRegisterRequest struct {
+	CredentialType  string `json:"credentialType"`
+	CredentialValue string `json:"credentialValue"`
+	Name            string `json:"name"`
+	Password        string `json:"password"`
+}
