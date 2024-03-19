46018d194ae16b35d4cea595f819704bb3bd53dd
bayazidsustami <bayazidsustamy@gmail.com>
2024-03-19 10:32:34 +0700

    feat : create validation

--- models/user/user_register_request.go
+++ models/user/user_register_request.go
@@ -1,8 +1,8 @@
 package user_model
 
 type UserRegisterRequest struct {
-	CredentialType  string `json:"credentialType"`
-	CredentialValue string `json:"credentialValue"`
-	Name            string `json:"name"`
-	Password        string `json:"password"`
+	CredentialType  string `json:"credentialType" validate:"required,oneof=email phone"`
+	CredentialValue string `json:"credentialValue" validate:"required"`
+	Name            string `json:"name" validate:"required,min=5,max=15"`
+	Password        string `json:"password" validate:"required,min=5,max=15"`
 }
