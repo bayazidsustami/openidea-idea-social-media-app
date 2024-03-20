98d1c869328c874bf21aa39e709013be09d5518d
bayazidsustami <bayazidsustamy@gmail.com>
2024-03-20 23:08:46 +0700

    feat : complete phone and email linked

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -2,13 +2,11 @@ package repository
 
 import (
 	"context"
-	"errors"
-	"log"
+	"database/sql"
 	"openidea-idea-social-media-app/customErr"
 	user_model "openidea-idea-social-media-app/models/user"
 
 	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -89,57 +87,125 @@ func (repository *UserRepositoryImpl) Login(ctx context.Context, conn *pgxpool.C
 }
 
 func (repository *UserRepositoryImpl) UpdateEmail(ctx context.Context, conn *pgxpool.Conn, userId int, email string) error {
+	tx, err := conn.Begin(ctx)
+	if err != nil {
+		return customErr.ErrorInternalServer
+	}
+	defer tx.Rollback(ctx)
+
+	GET_EMAIL := "SELECT email FROM users u WHERE u.user_id = $1"
+
+	var resEmail sql.NullString
+	err = tx.QueryRow(ctx, GET_EMAIL, userId).Scan(&resEmail)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			return customErr.ErrorBadRequest
+		} else {
+			return customErr.ErrorInternalServer
+		}
+	}
+
+	if resEmail.String != "" {
+		return customErr.ErrorBadRequest
+	}
+
+	GET_IS_EXISTED := "SELECT EXISTS (SELECT 1 FROM users WHERE email = $1)"
+	var isExistsEmail bool
+	err = tx.QueryRow(ctx, GET_IS_EXISTED, email).Scan(&isExistsEmail)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			return customErr.ErrorBadRequest
+		} else {
+			return customErr.ErrorInternalServer
+		}
+	}
+
+	if isExistsEmail {
+		return customErr.ErrorConflict
+	}
+
 	UPDATE_EMAIL := `
 		UPDATE users
-		SET email = CASE 
-				WHEN email IS NULL OR email = '' THEN $1
-				ELSE email
-				END
+		SET email = $1
 		WHERE user_id = $2
 		AND (email IS NULL OR email = '')
-		ON CONFLICT (email) DO NOTHING
 	`
-	res, err := conn.Exec(ctx, UPDATE_EMAIL, email, userId)
+	res, err := tx.Exec(ctx, UPDATE_EMAIL, email, userId)
 	if err != nil {
-		var pgErr *pgconn.PgError
-		if errors.As(err, &pgErr) {
-			log.Println(pgErr.Code)
-			log.Println(pgErr.Message)
+		if err == pgx.ErrNoRows {
+			return customErr.ErrorBadRequest
+		} else {
+			return customErr.ErrorInternalServer
 		}
-		return err
 	}
 
 	if res.RowsAffected() == 0 {
 		return customErr.ErrorNotFound
 	}
+
+	tx.Commit(ctx)
 
 	return nil
 }
 
 func (repository *UserRepositoryImpl) UpdatePhone(ctx context.Context, conn *pgxpool.Conn, userId int, phone string) error {
-	UPDATE_PHONE := `
+	tx, err := conn.Begin(ctx)
+	if err != nil {
+		return customErr.ErrorInternalServer
+	}
+	defer tx.Rollback(ctx)
+
+	GET_PHONE := "SELECT phone FROM users u WHERE u.user_id = $1"
+
+	var resPhone sql.NullString
+	err = tx.QueryRow(ctx, GET_PHONE, userId).Scan(&resPhone)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			return customErr.ErrorBadRequest
+		} else {
+			return customErr.ErrorInternalServer
+		}
+	}
+
+	if resPhone.String != "" {
+		return customErr.ErrorBadRequest
+	}
+
+	GET_IS_EXISTED := "SELECT EXISTS (SELECT 1 FROM users WHERE phone = $1)"
+	var isExistsPhone bool
+	err = tx.QueryRow(ctx, GET_IS_EXISTED, phone).Scan(&isExistsPhone)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			return customErr.ErrorBadRequest
+		} else {
+			return customErr.ErrorInternalServer
+		}
+	}
+
+	if isExistsPhone {
+		return customErr.ErrorConflict
+	}
+
+	UPDATE_EMAIL := `
 		UPDATE users
-		SET phone = CASE 
-				WHEN phone IS NULL OR phone = '' THEN $1
-				ELSE phone
-				END
+		SET phone = $1
 		WHERE user_id = $2
 		AND (phone IS NULL OR phone = '')
-		ON CONFLICT (phone) DO NOTHING
 	`
-	res, err := conn.Exec(ctx, UPDATE_PHONE, phone, userId)
+	res, err := tx.Exec(ctx, UPDATE_EMAIL, phone, userId)
 	if err != nil {
-		var pgErr *pgconn.PgError
-		if errors.As(err, &pgErr) {
-			log.Println(pgErr.Code)
-			log.Println(pgErr.Message)
+		if err == pgx.ErrNoRows {
+			return customErr.ErrorBadRequest
+		} else {
+			return customErr.ErrorInternalServer
 		}
-		return err
 	}
 
 	if res.RowsAffected() == 0 {
 		return customErr.ErrorNotFound
 	}
+
+	tx.Commit(ctx)
 
 	return nil
 }
