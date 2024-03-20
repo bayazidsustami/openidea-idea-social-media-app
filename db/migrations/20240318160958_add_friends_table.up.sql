d3fba111bb3bc5c95c50d842366b41de56c3e0a9
bayazidsustami <bayazidsustamy@gmail.com>
2024-03-20 08:36:42 +0700

    chore : create composite indexes

--- db/migrations/20240318160958_add_friends_table.up.sql
+++ db/migrations/20240318160958_add_friends_table.up.sql
@@ -6,3 +6,5 @@ CREATE TABLE IF NOT EXISTS friends (
   FOREIGN KEY (user_id_requester) REFERENCES users (user_id) ON DELETE CASCADE,
   FOREIGN KEY (user_id_accepter) REFERENCES users (user_id) ON DELETE CASCADE
 );
+
+CREATE UNIQUE INDEX unique_requester_accepter_index ON friends (user_id_requester, user_id_accepter)
