2357594256a82b8648f67b14bdb4a8fcae36727d
bayazidsustami <bayazidsustamy@gmail.com>
2024-03-23 10:16:55 +0700

    chore : change user_id type

--- db/migrations/20240318160958_add_friends_table.up.sql
+++ db/migrations/20240318160958_add_friends_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS friends (
   friend_id SERIAL PRIMARY KEY NOT NULL,
-	user_id_requester INTEGER NOT NULL,
-	user_id_accepter INTEGER NOT NULL,
+	user_id_requester UUID NOT NULL,
+	user_id_accepter UUID NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (user_id_requester) REFERENCES users (user_id) ON DELETE CASCADE,
   FOREIGN KEY (user_id_accepter) REFERENCES users (user_id) ON DELETE CASCADE,
