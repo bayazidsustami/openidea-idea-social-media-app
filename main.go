0ff5a1782eb9850de7e847e87b53f5d65eac3713
bayazidsustami <bayazidsustamy@gmail.com>
2024-03-23 16:18:47 +0700

    chore : enable prefork default

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	var port string
 	var prefork bool
 	flag.StringVar(&port, "port", "8080", "application port")
-	flag.BoolVar(&prefork, "prefork", false, "enable prefork")
+	flag.BoolVar(&prefork, "prefork", true, "enable prefork")
 	flag.Parse()
 
 	app.StartFiberApp(port, prefork)
